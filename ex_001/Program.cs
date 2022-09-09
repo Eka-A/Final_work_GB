028ab638b50c298dfd864d7299d495b4403a2ffc
Ekaterina Anikina <reityan97@mail.ru>
2022-09-09 18:09:34 +0300

    добавлены переменные и метод печати массива

---
+++ ex_001/Program.cs
@@ -0,0 +1,22 @@
+﻿int size = 4;
+string[] array = new string[size];
+int maxLength = 3;
+array[0] = "Ru";
+array[1] = "321";
+array[2] = "Kazan";
+array[3] = "LA";
+
+PrintArray(array);
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"'{array[i]}',");
+    }
+    Console.Write("\b");
+    Console.Write("]");
+    Console.WriteLine();
+}
